f7242f099afeefa129296a8905340c122ec1bcb2
aurel fontaine <ajfame11@gmail.com>
2020-05-01 21:18:32 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,5 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+<<<<<<< HEAD
 require_relative './fancy_dance.rb'
 class Dancer
     include Dance
@@ -9,6 +10,10 @@ class Dancer
   def initialize(name)
     @name = name
   end
+=======
+
+class Dancer
+>>>>>>> c13b0727fb9cb00577e418abd05ec9a10a6bcf49
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,5 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+<<<<<<< HEAD
 require_relative './fancy_dance.rb'
 class Kid
     include Dance
@@ -9,6 +10,10 @@ class Kid
   def initialize(name)
     @name = name
   end
+=======
+
+class Kid
+>>>>>>> c13b0727fb9cb00577e418abd05ec9a10a6bcf49
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
